2aab3b56079c7d3e57ff476e243a408226a7dead
Aleks B <aleks.balev7899@gmail.com>
2023-05-24 09:01:50 +0200

    clean url and method based routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,65 +8,63 @@ import (
 	"strconv"
 
 	"snippetbox.aleksbalev.net/internal/models"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
-		
-		snippets, err := app.snippets.Latest()
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-		data := app.newTemplateData(r)
-		data.Snippets = snippets
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-		app.render(w, http.StatusOK, "home.tmpl.html", data)
+	app.render(w, http.StatusOK, "home.tmpl.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
-        app.notFound(w)
-        return
-    }
+	params := httprouter.ParamsFromContext(r.Context())
 
-		snippet, err := app.snippets.Get(id)
-		if err != nil {
-			if errors.Is(err, models.ErrNoRecord) {
-				app.notFound(w)
-			} else {
-				app.serverError(w, err)
-			}
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+			app.notFound(w)
 			return
+	}
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
 		}
+		return
+	}
 
-		data := app.newTemplateData(r)
-		data.Snippet = snippet
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-		app.render(w, http.StatusOK, "view.tmpl.html", data)
+	app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
-		title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := 7
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-		id, err := app.snippets.Insert(title, content, expires)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-		http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
