4a6c1bf399f248722c6ff0076e2ba421e9a4ff0f
Aleks B <aleks.balev7899@gmail.com>
2023-06-14 07:21:59 +0200

    added debug mode

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"runtime/debug"
 	"time"
@@ -15,11 +16,15 @@ import (
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
   if err := app.errorLog.Output(2, trace); err != nil {
-    app.serverError(w, err)
+    log.Fatal(err)
+  }
+
+  if app.debug {
+    http.Error(w, trace, http.StatusInternalServerError)
     return
   }
 
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+  http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
